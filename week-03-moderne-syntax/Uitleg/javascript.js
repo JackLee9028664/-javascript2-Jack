e353e7ce38a2af813c32881451a7ebd41d879dae
JackLee9028664 <9028664@student.zadkine.nl>
2026-09-23 13:39:00 +0200

    week3 eindopdracht

--- week-03-moderne-syntax/Uitleg/javascript.js
+++ week-03-moderne-syntax/Uitleg/javascript.js
@@ -21,3 +21,18 @@ const fruits =['apple', 'banaan', 'perzik']
 for(let fruit of fruits) {
      Getname.innerHTML += fruit + "<br>";
 }
+
+
+
+let title = document.querySelector("#title");
+let button = document.querySelector("#btn");
+let section = document.querySelector("#section");
+
+button.addEventListener("click", () => {
+title.textContent = "ik heb geclickt" 
+title.classList.toggle("active");
+
+const p = document.createElement("p")
+p.textContent = "voeg een paragraaf toe"
+section.appendChild (p)
+})
--- week-03-moderne-syntax/eindopdracht/script.js
+++ week-03-moderne-syntax/eindopdracht/script.js
@@ -1,3 +1,46 @@
 // Stap 1: Selecteer het formulier en de profielenlijst
+let profile_form = document.querySelector("#profile-form")
+let profiles_list =  document.querySelector("#profiles-list")
+let button = document.querySelector("#button")
+
+
+let delete1 = document.querySelector("#delete1")
+let delete2 = document.querySelector("#delete2")
+let delete3 = document.querySelector("#delete3")
+
+
+
 // Stap 2: Luister naar het submit-event, lees de invoervelden uit met .value en toon een profielkaart met innerHTML +=
+button.addEventListener("click", () => {
+     event.preventDefault();
+     let role  = document.querySelector("#role").value
+     let department = document.querySelector("#department").value
+     let naam = document.querySelector("#naam").value
+
+
+  
+    profiles_list.innerHTML +=   `<li> ${naam}  ${role}  ${department} </li>`
+  
+  
+})
+
+
 // Stap 3 (bonus): Voeg een verwijderknop toe aan elke kaart
+
+delete1.addEventListener("click", () => {
+  event.preventDefault();
+  
+  naam.value = "";
+});
+
+delete2.addEventListener("click", () => {
+  event.preventDefault();
+  
+  role.value = "";
+});
+
+delete3.addEventListener("click", () => {
+  event.preventDefault();
+  
+  department.value = "";
+});
