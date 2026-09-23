cc3158b0ffb195b05835ea87c0d4b724bccb4d39
JackLee9028664 <9028664@student.zadkine.nl>
2026-09-23 14:45:18 +0200

    week3 fixed eindopdrachht

--- week-03-moderne-syntax/eindopdracht/script.js
+++ week-03-moderne-syntax/eindopdracht/script.js
@@ -1,46 +1,40 @@
 // Stap 1: Selecteer het formulier en de profielenlijst
-let profile_form = document.querySelector("#profile-form")
-let profiles_list =  document.querySelector("#profiles-list")
-let button = document.querySelector("#button")
-
-
-let delete1 = document.querySelector("#delete1")
-let delete2 = document.querySelector("#delete2")
-let delete3 = document.querySelector("#delete3")
+let profile_form = document.querySelector("#profile-form");
+let profiles_list = document.querySelector("#profiles-list");
+let button = document.querySelector("#button");
 
 
 
 // Stap 2: Luister naar het submit-event, lees de invoervelden uit met .value en toon een profielkaart met innerHTML +=
-button.addEventListener("click", () => {
-     event.preventDefault();
-     let role  = document.querySelector("#role").value
-     let department = document.querySelector("#department").value
-     let naam = document.querySelector("#naam").value
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  let role = document.querySelector("#role").value;
+  let department = document.querySelector("#department").value;
+  let naam = document.querySelector("#naam").value;
 
-if (role == "" || department == "" || naam == "") {
-    alert("geen informatie")
+  if (role == "" || department == "" || naam == "") {
+    alert("geen informatie");
   } else {
-   profiles_list.innerHTML +=   `<li> ${naam}  ${role}  ${department} </li>`
+    profiles_list.innerHTML += `
+   <article>
+   <h2>${naam} </h2>
+   <p>Role: ${role} </p>
+   <p>Department: ${department}</p>
+   <button id="delete">Verwijder</button>
+   
+   </article>
+  
+   
+   `;
   }
-})
+  profile_form.reset();
 
+  document.querySelector("#delete").addEventListener("click", (e) => {
+  e.preventDefault();
+  profiles_list.remove();
+});
+
+});
 
 // Stap 3 (bonus): Voeg een verwijderknop toe aan elke kaart
 
-delete1.addEventListener("click", () => {
-  event.preventDefault();
-  
-  naam.value = "";
-});
-
-delete2.addEventListener("click", () => {
-  event.preventDefault();
-  
-  role.value = "";
-});
-
-delete3.addEventListener("click", () => {
-  event.preventDefault();
-  
-  department.value = "";
-});
